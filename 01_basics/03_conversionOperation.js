049376405131ab3115b8db64d87689fb71b89477
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-23 17:12:43 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let age=undefined
+console.log(typeof age)
+console.log(typeof (age))
+let valueInNumber=Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"33"=>33
+//"33abc"=>NaN(not a number)
+//true=>1
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn="anand"
+console.log(typeof isLoggedIn)
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=>true
+//""=>false
+//"abs"=>true
+let someNumber=23
+let anotherNumber=String(someNumber)
+console.log(anotherNumber)
+console.log(typeof anotherNumber)

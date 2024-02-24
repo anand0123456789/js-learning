f373864e9bcb59ee836c4d59753a984c00e3e328
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-24 17:46:04 +0000

    conversion of string problem had completed

---
+++ 01_basics/04_stringConversion.js
@@ -0,0 +1,30 @@
+/* console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)  */
+let str1="anand"
+let str2=" pandey"
+let str3=str1+str2
+// console.log("2"+2); =>22
+//console.log(2+"2");=> 22
+// console.log("2"+"2");=>22
+// console.log(1+1+"2"); =>22
+// console.log("2"+1+1); =>211
+console.log("2"+1+"1");
+console.log(2+"1"+1);
+console.log(true);
+console.log(true+1);
+console.log(+true);
+// console.log(true+);=>give an error
+
+console.log(+"");
+let x=3n
+let y=x++
+let z=++x
+console.log(y);
+console.log(x);
+console.log(z);
+
+
+

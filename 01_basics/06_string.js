89dc16cf577d346a0081914f386bb45bfedb1710
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-25 18:04:18 +0000

    string function study

---
+++ 01_basics/06_string.js
@@ -0,0 +1,19 @@
+const name="anand"
+const repocount=50
+//console.log(name+repocount+" value");
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+//``=>backticks
+const gamename=new String('anandkumar')
+// console.log(gamename[0]);
+//console.log(gamename.__proto__);
+//console.log(gamename.charAt(5));
+//console.log(gamename.indexOf('u'));
+const newn=gamename.substring(2,5)
+console.log(newn);
+const anotherS=gamename.slice(-1,8)
+console.log(anotherS)
+const nnn="      anand             "
+console.log(nnn.trim())
+const url="https//jscrip.com/anand%20kumar"
+console.log(url.replace('%20','_'));
+console.log(gamename.big())

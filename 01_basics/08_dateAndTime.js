d3d5fce548d703a9ea4a23025faca5aa1a95888f
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-25 19:04:59 +0000

    dateail about date and month

---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,11 @@
+//let mydate=new Date()
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());=>Sun Feb 25 2024
+//console.log(mydate.toISOString());=>2024-02-25T18:47:33.656Z
+//console.log(mydate.toLocaleDateString());=>2/25/2024
+//let createMydate=new Date(2024,2,3,5,3)
+//console.log(createMydate.toString());
+//let myDate=new Date("2024-3-12")=>Tue Mar 12 2024 00:00:00 GMT+0000 (Coordinated Universal Time)
+//console.log(myDate.toString());
+let presentTime = Date.now()
+console.log(presentTime);

a21dadd0d3e1910e01790d9fb9c3ca296df7e4ee
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-26 07:54:50 +0000

    destructuring of object

---
+++ 02_basics/05_object.js
@@ -0,0 +1,11 @@
+const course={
+    courseName:"javascript",
+    courseType:"full",
+    instructor:"hitesh"
+}
+const {courseName:N}=course
+console.log(N);
+//{
+// "course":"name"
+// "type":"part"      =>json type or [{},{},{}]
+//}

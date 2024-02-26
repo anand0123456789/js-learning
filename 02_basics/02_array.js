ab1ab79743add0d66eaed9ed815e76f031748a66
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-26 06:21:44 +0000

    array's basic done

---
+++ 02_basics/02_array.js
@@ -0,0 +1,7 @@
+let m_heroes=['thor','ironman','spiderman','captain']
+let d_heroes=['joker','superman','batman','aquaman']
+let all=m_heroes.concat(d_heroes)
+//console.log(all)=>concat add 2 array in single another array
+//console.log(all.at(5));
+//console.log(all.copyWithin(2,6,7));
+console.log(Array.from("hitesh"));

e6af677a1f751575ea365fc9ac5d28f8bc80e719
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-02-26 06:55:29 +0000

    basics of object done

---
+++ 02_basics/03_object.js
@@ -0,0 +1,8 @@
+let anObject={
+    name:"anand",
+    phone:9458345329,
+    email:"unstable@gmail.com",
+    isLoggedIn:false
+}
+//console.log(anObject["email"])
+console.log(`a person whose name is ${anObject["name"]} and his calling number is ${anObject["phone"]}`);

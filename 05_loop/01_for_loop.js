c7d7afd710ec76ed46a5b0d9186defdbeee6e133
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-03-01 20:46:52 +0000

    for loop done

---
+++ 05_loop/01_for_loop.js
@@ -0,0 +1,11 @@
+// const array=[48,47,45,45]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+const num=[50,51,52,49,48,47];
+num.sort();
+for(let i=0;  i<num.length; i++){
+    console.log(num[i]);
+}
+//break & continue

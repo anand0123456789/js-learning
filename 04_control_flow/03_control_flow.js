1405bd7c70f852c6e9167bffb0af6d773e766eb3
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-03-01 18:43:18 +0000

    truthy and falsy

---
+++ 04_control_flow/03_control_flow.js
@@ -0,0 +1,10 @@
+const user="anand@kumar.ai"
+if(user){
+    console.log("user got");
+}
+else{
+    console.log("not any user found");
+}
+// falsy value
+// 0,false,-0,bigint 0n,"",null,undefined,NaN
+// false==0=>true

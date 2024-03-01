64b44ff311a5cd9335f2150e051611e11a16a74a
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-03-01 18:13:30 +0000

    if else condition

---
+++ 04_control_flow/01_control_flow.js
@@ -0,0 +1,15 @@
+// if(condition){
+
+// }
+// if(2==="2"){
+//     console.log("executed");
+// }
+// else{
+//     console.log("chal be")
+// 
+// const num=[50,51,52,49,48,47];
+// num.sort();
+
+// if(num[2]<50){
+//     console.log("element is less than 50")
+// }

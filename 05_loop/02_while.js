f111e01a302367f45abc3c1548447fcb76281121
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-03-02 18:36:08 +0000

    while and do while completed

--- 05_loop/02_while.js
+++ 05_loop/02_while.js
@@ -1,6 +1,12 @@
 let i=0;
 let arr=[0,48,45,52]
-while(i<arr.length){
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i=i+1;
+// }
+do{
     console.log(arr[i]);
-    i=i+1;
+    i++;
 }
+while(i<arr.length)
+console.log(Math.pow(2,5))

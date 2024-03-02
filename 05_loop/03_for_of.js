7a1aa8adcda81accb95458f175c8f8bf6ad7c053
anand kumar <105169150+anand0123456789@users.noreply.github.com>
2024-03-02 20:03:59 +0000

    forof,forin and dor each done

---
+++ 05_loop/03_for_of.js
@@ -0,0 +1,11 @@
+// array specific loop
+const arr=[1,2,3,4,5,6];
+// for (const i of arr) {
+//     console.log(i);
+// }
+// for (const key in arr) {
+//   console.log(arr[key]);
+// }
+arr.forEach(function(item){
+    console.log(item);
+})
